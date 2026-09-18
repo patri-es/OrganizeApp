15ce16668fc889f7ac825b718ca3a5ef063abc7e
Patri <devpatrigus@gmail.com>
2026-09-18 11:01:31 +0200

    componente Navbar para el front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
     return (
         <>
-            <h1>
-                Hola Mundo!
-            </h1>
-            <h2>Esta es mi primera web con React</h2>
+          <Navbar />
         </>
     )
 }
