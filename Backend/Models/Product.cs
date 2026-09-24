4af8946cb675a7864624fd4c628fdf6f5e9fb6e2
Patri <devpatrigus@gmail.com>
2026-09-24 16:40:11 +0200

    dejo CategoryId como nulable

--- Backend/Models/Product.cs
+++ Backend/Models/Product.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 
-
 namespace Backend.Models;
 
 public class Product
@@ -11,7 +10,7 @@ public class Product
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public DateTime CreationDate { get; set; } = DateTime.Now;
-    public int CategoryId { get; set; }
+    public int? CategoryId { get; set; }
     public bool Available { get; set; } = true;
     public int Stock { get; set; } = 0;
 
